b6b2cb22416847b62c947f24e60de49967d25230
priscillavassallo <priscillavassallo96@gmail.com>
2026-02-07 19:16:42 -0300

    feat: adiciona servidor

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import { router } from "./routes";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+const PORT = 3333;
+
+app.listen(PORT, () => {
+  console.log(`Server rodando em http://localhost:${PORT}`);
+});
